1bab55df8bed50a4c648aa945141b9ee5abe0550
allan.burdajewicz@gmail.com <allan.burdajewicz@gmail.com@ba48e07c-870f-fb0f-f729-422f4b6003c2>
2011-09-26 14:55:55 +0000

    Class Graph completed Class GraphI completed Submitted to Code Review & Inspection

--- trunk/src/com/csse7034/zerg/c1/sorting/GraphI.java
+++ trunk/src/com/csse7034/zerg/c1/sorting/GraphI.java
@@ -1,7 +1,8 @@
 /**
+ * GraphI: Interface to implement as a Graph
  * 
- * @author Allan BURDAJEWICZ
- *
+ * @see		Class Graph
+ * @author 	Allan BURDAJEWICZ
  */
 
 package com.csse7034.zerg.c1.sorting;
@@ -10,50 +11,44 @@ package com.csse7034.zerg.c1.sorting;
 public interface GraphI {
 
 	/**
-	 * 
-	 * @param name
+	 * Add a node in the Graph
+	 * @param name		The name of the node
 	 */
 	public void addNode(String name);
 	
 	/**
-	 * 
-	 * @param src
-	 * @param dest
+	 * Add en edge from the a source node to a destination node
+	 * @param src		source node
+	 * @param dest		destination node
 	 * @return
 	 */
 	public String addEdge(String src, String dest);
 	
 	/**
-	 * 
-	 * @return
+	 * Return the depth of the graph (Maximum)
+	 * @return	the maximum depth of the Graph
 	 */
 	public int getDepth();
 	
 	/**
-	 * 
-	 * @param level
-	 * @return
+	 * Return an array with all the node at the given level in the Graph
+	 * @param level		The level
+	 * @return			The array with all the node at this level
 	 */
 	public NodeI [] getNodesAtLevel(int level);
 	
 	/**
-	 * 
-	 * @param name
-	 * @return
+	 * Given the name of a node, return if the graph contains the node
+	 * @param name		The name of the node to search for
+	 * @return			The graph contains the node "name"
 	 */
 	public boolean contains(String name);
 	
 	/**
-	 * 
-	 * @param name
-	 * @return
+	 * Given the name of a node, return the index of the node in the array "nodes"
+	 * @param name		The name of the node
+	 * @return			The index of the node in the array <nodes>
 	 */
 	public int indexOf(String name);
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public String toString();
 	
 }

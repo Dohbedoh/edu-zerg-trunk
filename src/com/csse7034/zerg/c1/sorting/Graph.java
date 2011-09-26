9c47421974ea97083044b5dcce2b72d67a3feb88
allan.burdajewicz <allan.burdajewicz@gmail.com>
2011-09-26 00:49:37 +0000

    Package Architecture Complete Interface GraphI & Graph Complete

---
+++ src/com/csse7034/zerg/c1/sorting/Graph.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ * @author Allan BURDAJEWICZ
+ *
+ */
+
+package com.csse7034.zerg.c1.sorting;
+
+
+public class Graph implements GraphI{
+
+	/** */
+	private NodeI [] nodes;
+	
+	/** */
+	private int [][] edges;
+	
+	
+	public Graph()
+	{
+		
+	}
+	
+	/**
+	 * 
+	 * @param name
+	 * @return
+	 */
+	private int findLevel(String name)
+	{
+		return 0;
+	}
+
+	/**
+	 * 
+	 */
+	private void resize()
+	{
+		
+	}
+
+	@Override
+	public void addNode(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public String addEdge(String src, String dest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public int getDepth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public NodeI[] getNodesAtLevel(int level) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public boolean contains(String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public int indexOf(String name) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
---
+++ src/com/csse7034/zerg/c1/sorting/UI.java
@@ -0,0 +1,5 @@
+package com.csse7034.zerg.c1.sorting;
+
+public class UI {
+
+}

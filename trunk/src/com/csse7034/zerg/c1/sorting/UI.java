2a8ee46afe00ef9543002bd0e09173a205973b69
dstan89@gmail.com <dstan89@gmail.com@ba48e07c-870f-fb0f-f729-422f4b6003c2>
2011-09-27 05:37:20 +0000

    Fixed parse() in parser, added new lines in UI.

--- trunk/src/com/csse7034/zerg/c1/sorting/UI.java
+++ trunk/src/com/csse7034/zerg/c1/sorting/UI.java
@@ -1,5 +1,21 @@
 package com.csse7034.zerg.c1.sorting;
+import java.util.Scanner;
 
 public class UI {
-
+	public static void main() {
+		Graph graph = new Graph();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter file path or file name :");
+		String fName = sc.nextLine();
+		
+		Parser p = new Parser();
+		
+		if (p.Parse(graph, fName) == "ok") {
+			//Do sorting
+		} else {
+			System.out.println(p.Parse(graph, fName));
+		}
+		
+	}
 }

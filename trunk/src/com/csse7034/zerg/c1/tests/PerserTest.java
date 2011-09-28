6ff5420810ec77cfeea90d909474e1c53168bbd6
pmminov@gmail.com <pmminov@gmail.com@ba48e07c-870f-fb0f-f729-422f4b6003c2>
2011-09-28 02:22:50 +0000

    Unit tests created

---
+++ trunk/src/com/csse7034/zerg/c1/tests/PerserTest.java
@@ -0,0 +1,14 @@
+package com.csse7034.zerg.c1.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PerserTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
